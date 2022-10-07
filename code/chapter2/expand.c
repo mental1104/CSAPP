bb5d75b0cb144277e408521311a3f5e66ba4eba5
mental1104 <mental1104@gmail.com>
2022-10-07 11:23:46 +0800

    modify expand code

--- code/chapter2/expand.c
+++ code/chapter2/expand.c
@@ -1,13 +1,19 @@
-short sx = -12345; /* -12345 */
-unsigned short usx = sx; /* 53191 */
-int x = sx; /* -12345 */
-unsigned ux = usx; /* 53191 */
+#include <stdio.h>
+#include "show_bytes.h"
 
-printf("sx = %d:\t", sx);
-show_bytes((byte_pointer) &sx, sizeof(short));
-printf("usx = %u:\t", usx);
-show_bytes((byte_pointer) &usx, sizeof(unsigned short));
-printf("x = %d:\t", x);
-show_bytes((byte_pointer) &x, sizeof(int));
-printf("ux = %u:\t", ux);
-show_bytes((byte_pointer) &ux, sizeof(unsigned));
+int main(){
+    short sx = -12345; /* -12345 */
+    unsigned short usx = sx; /* 53191 */
+    int x = sx; /* -12345 */
+    unsigned ux = usx; /* 53191 */
+
+    printf("sx = %d:\t", sx);
+    show_bytes((byte_pointer) &sx, sizeof(short));
+    printf("usx = %u:\t", usx);
+    show_bytes((byte_pointer) &usx, sizeof(unsigned short));
+    printf("x = %d:\t", x);
+    show_bytes((byte_pointer) &x, sizeof(int));
+    printf("ux = %u:\t", ux);
+    show_bytes((byte_pointer) &ux, sizeof(unsigned));
+}
+
